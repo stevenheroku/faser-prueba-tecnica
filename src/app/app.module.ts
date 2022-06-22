6dc68ff74eb73aa0990e0273a6e9c6c6dc0c1cfc
stevenheroku <73918465+stevenheroku@users.noreply.github.com>
2022-06-22 14:01:35 -0600

    angularMaterial
    Instalar angular material para su funcionamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import {FormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCheckboxModule } from '@angular/material';
+
 
 @NgModule({
 	declarations: [
@@ -10,9 +20,17 @@ import { AppService } from './app.service';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+	MatSortModule,
+	ReactiveFormsModule,
+	MatCheckboxModule
 	],
-	providers: [ AppService ],
+	providers: [  ],
 	bootstrap: [ AppComponent ]
 })
 export class AppModule { }
